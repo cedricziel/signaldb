cd77baec4afc5a0498da76bb8b199a6e6b4c5c1c
Cedric Ziel <cedric@cedric-ziel.com>
2024-12-21 11:38:22 +0100

    feat: add queue primitives

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -2,3 +2,4 @@ pub mod config;
 pub mod dataset;
 pub mod model;
 pub mod persistence;
+pub mod queue;
--- src/common/src/persistence/mod.rs
+++ src/common/src/persistence/mod.rs
@@ -1,46 +1,45 @@
 use std::sync::Arc;
 
+use arrow_array::RecordBatch;
 use datafusion::{
     datasource::{file_format::parquet::ParquetFormat, listing::ListingOptions},
     error::DataFusionError,
-    execution::object_store::ObjectStoreUrl,
+    parquet::{
+        arrow::{async_writer::ParquetObjectWriter, AsyncArrowWriter},
+        file::properties::{WriterProperties, WriterVersion},
+    },
     prelude::SessionContext,
 };
-use object_store::local::LocalFileSystem;
+use object_store::{local::LocalFileSystem, path::Path, ObjectStore};
 
 use crate::config::CONFIG;
 
-pub async fn create_df_session() -> Result<SessionContext, DataFusionError> {
-    let config = CONFIG.get().expect("Failed to load configuration");
-    let storage_url = config.default_storage_url();
-    let storage_prefix = config.default_storage_prefix();
+pub async fn create_session_context() -> Result<SessionContext, DataFusionError> {
+    let storage_url = CONFIG
+        .get()
+        .unwrap()
+        .default_storage_url()
+        .parse()
+        .expect("Failed to parse storage URL");
 
-    let file_format = ParquetFormat::default().with_enable_pruning(true);
-    let listing_options = ListingOptions::new(Arc::new(file_format))
-        .with_table_partition_cols(vec![])
-        .with_file_extension(".parquet")
-        .with_collect_stat(true);
+    let storage_prefix = CONFIG.get().unwrap().default_storage_prefix();
 
-    // if storage_prefix is an absolute path, use as is, if it's a relative path,
-    // get the current working directory and append the relative path.
-    let storage_prefix = if storage_prefix.starts_with("/") {
-        storage_prefix
-    } else {
-        let cwd = std::env::current_dir().expect("Failed to get current working directory");
-        cwd.join(storage_prefix).to_string_lossy().to_string()
-    };
-
-    let object_store_url = ObjectStoreUrl::parse(storage_url.clone()).unwrap();
     let object_store = LocalFileSystem::new_with_prefix(storage_prefix.clone()).expect(
         format!(
-            "Failed to create object store for {}",
-            storage_prefix.as_str()
+            "Failed to create local filesystem with prefix {}",
+            storage_prefix
         )
         .as_str(),
     );
 
     let ctx = SessionContext::new();
-    ctx.register_object_store(object_store_url.as_ref(), Arc::new(object_store));
+    ctx.runtime_env()
+        .register_object_store(&storage_url, Arc::new(object_store));
+
+    let file_format = ParquetFormat::default().with_enable_pruning(true);
+    let listing_options =
+        ListingOptions::new(Arc::new(file_format)).with_file_extension(".parquet");
+
     ctx.register_listing_table(
         "traces",
         format!("{}/ds/traces/", storage_url),
@@ -51,4 +50,28 @@ pub async fn create_df_session() -> Result<SessionContext, DataFusionError> {
     .await?;
 
     Ok(ctx)
+}
+
+pub async fn write_batch_to_object_store(
+    object_store: Arc<dyn ObjectStore>,
+    path: &str,
+    batch: RecordBatch,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let path = object_store::path::Path::from(path);
+
+    let props = WriterProperties::builder()
+        .set_writer_version(WriterVersion::PARQUET_2_0)
+        .build();
+
+    let schema = batch.schema();
+
+    let object_store_writer = ParquetObjectWriter::new(object_store.clone(), Path::from(path));
+
+    let mut arrow_writer = AsyncArrowWriter::try_new(object_store_writer, schema, Some(props))
+        .expect("Error creating parquet writer");
+
+    arrow_writer.write(&batch).await?;
+    arrow_writer.close().await?;
+
+    Ok(())
 }
