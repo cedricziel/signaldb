4bcba84c855180a8d13d5a3f206609e39edc8d9d
Cedric Ziel <cedric@cedric-ziel.com>
2025-01-12 00:35:00 +0100

    fix: extract router state correctly for queue types

--- src/router/src/lib.rs
+++ src/router/src/lib.rs
@@ -2,18 +2,31 @@ use std::sync::Arc;
 
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 use common::queue::Queue;
-use tokio::sync::Mutex;
 
 mod endpoints;
 
+pub trait RouterState: std::fmt::Debug + Clone + Send + Sync + 'static {
+    type Q: Queue;
+
+    fn queue(&self) -> &Self::Q;
+}
+
 /// RouterState holds any shared state that needs to be accessed by route handlers
 #[derive(Clone, Debug)]
-pub struct RouterState {
-    queue: Arc<dyn Queue>,
+pub struct InMemoryStateImpl {
+    queue: Arc<common::queue::InMemoryQueue>,
+}
+
+impl RouterState for InMemoryStateImpl {
+    type Q = common::queue::InMemoryQueue;
+
+    fn queue(&self) -> &Self::Q {
+        &self.queue
+    }
 }
 
 /// Create a new router instance with all routes configured
-pub fn create_router(state: RouterState) -> Router<RouterState> {
+pub fn create_router<S: RouterState>(state: S) -> Router<S> {
     Router::new()
         .with_state(state)
         .route("/health", get(health_check))
